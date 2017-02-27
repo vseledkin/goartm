beb102db2c7ba71c59f6d71fbd93b97ac0b44bad
viacheslav seledkin <vseledkin@work.local>
2017-02-27 12:49:41 +0300

    correct memory management and pointer syntax

--- artm.go
+++ artm.go
@@ -1,10 +1,13 @@
 package goartm
 
 // #cgo LDFLAGS: libartm.dylib
+// #include <stdlib.h>
 // #include "c_interface.h"
 import "C"
 import (
 	"fmt"
+
+	"unsafe"
 
 	"github.com/golang/protobuf/proto"
 )
@@ -21,15 +24,55 @@ var ARTM_ERRORS = []string{
 	"ARTM_DISK_WRITE_ERROR",
 }
 
+func New(conf interface{}) interface{} {
+	switch t := conf.(type) {
+	case *MasterModelConfig:
+		var MasterModelConfig_PwtName string = Default_MasterModelConfig_PwtName
+		var MasterModelConfig_NwtName string = Default_MasterModelConfig_NwtName
+		var MasterModelConfig_ReuseTheta bool = Default_MasterModelConfig_ReuseTheta
+		var MasterModelConfig_OptForAvx bool = Default_MasterModelConfig_OptForAvx
+		var MasterModelConfig_CacheTheta bool = Default_MasterModelConfig_CacheTheta
+
+		t.NwtName = &MasterModelConfig_NwtName
+		t.PwtName = &MasterModelConfig_PwtName
+		t.ReuseTheta = &MasterModelConfig_ReuseTheta
+		t.OptForAvx = &MasterModelConfig_OptForAvx
+		t.CacheTheta = &MasterModelConfig_CacheTheta
+		return t
+	case *GetDictionaryArgs:
+		return t
+	case *ImportModelArgs:
+		return t
+	case *ImportDictionaryArgs:
+		return t
+	default:
+		panic("unsupported type")
+	}
+}
+
+func ArtmGetLastErrorMessage() error {
+	err := C.ArtmGetLastErrorMessage()
+	defer C.free(unsafe.Pointer(err))
+	errorStr := C.GoString(err)
+	if len(errorStr) > 0 {
+		return fmt.Errorf("%s", errorStr)
+	}
+	return nil
+}
+
 //ArtmCreateMasterModel create master model
 func ArtmCreateMasterModel(config *MasterModelConfig) (int, error) {
 	message, err := proto.Marshal(config)
 	if err != nil {
 		return 0, fmt.Errorf("Protobuf MasterModelConfig marshaling error: %s", err)
 	}
-	masterID := C.int(C.ArtmCreateMasterModel(C.int64_t(len(message)), C.CString(string(message))))
+	masterID := C.int(C.ArtmCreateMasterModel(C.int64_t(len(message)), (*C.char)(unsafe.Pointer(&message[0]))))
 	if masterID < 0 {
 		return 0, fmt.Errorf("Create Master error: %s", ARTM_ERRORS[-masterID])
+	}
+	err = ArtmGetLastErrorMessage()
+	if err != nil {
+		return 0, err
 	}
 	return int(masterID), nil
 }
@@ -40,13 +83,13 @@ func ArtmImportModel(masterModelID int, config *ImportModelArgs) error {
 	if err != nil {
 		return fmt.Errorf("Protobuf ImportModelArgs marshaling error: %s", err)
 	}
-	errorID := C.ArtmImportModel(C.int(masterModelID), C.int64_t(len(message)), C.CString(string(message)))
+	errorID := C.ArtmImportModel(C.int(masterModelID), C.int64_t(len(message)), (*C.char)(unsafe.Pointer(&message[0])))
 	if errorID < 0 {
 		fmt.Printf("Load model error: %s\n", ARTM_ERRORS[-errorID])
 	}
-	errorStr := C.GoString(C.ArtmGetLastErrorMessage())
-	if len(errorStr) > 0 {
-		return fmt.Errorf("Load model error: %s\n", errorStr)
+	err = ArtmGetLastErrorMessage()
+	if err != nil {
+		return err
 	}
 	return nil
 }
@@ -57,37 +100,78 @@ func ArtmDisposeMasterComponent(masterModelID int) error {
 	if errorID < 0 {
 		fmt.Errorf("Dispose model error %s\n", ARTM_ERRORS[-errorID])
 	}
+	err := ArtmGetLastErrorMessage()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ArtmImportDictionary(masterModelID int, conf *ImportDictionaryArgs) error {
+	message, err := proto.Marshal(conf)
+	if err != nil {
+		return fmt.Errorf("Protobuf ImportDictionaryArgs marshaling error: %s", err)
+	}
+	errorID := C.ArtmImportDictionary(C.int(masterModelID), C.int64_t(len(message)), (*C.char)(unsafe.Pointer(&message[0])))
+	if errorID < 0 {
+		fmt.Printf("Load dictionary error: %s\n", ARTM_ERRORS[-errorID])
+	}
+	err = ArtmGetLastErrorMessage()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 //ArtmRequestDictionary request dictionary
 func ArtmRequestDictionary(masterModelID int, conf *GetDictionaryArgs) (*DictionaryData, error) {
-
 	message, err := proto.Marshal(conf)
 	if err != nil {
 		return nil, fmt.Errorf("Protobuf GetDictionaryArgs marshaling error: %s", err)
 	}
-	messageLength := C.ArtmRequestDictionary(C.int(masterModelID), C.int64_t(len(message)), C.CString(string(message)))
-	errorStr := C.GoString(C.ArtmGetLastErrorMessage())
-	if len(errorStr) > 0 {
-		return nil, fmt.Errorf("Get model dictionary error: %s\n", errorStr)
+	p := unsafe.Pointer(&message[0])
+	messageLength := C.ArtmRequestDictionary(C.int(masterModelID), C.int64_t(len(message)), (*C.char)(p))
+	err = ArtmGetLastErrorMessage()
+	if err != nil {
+		return nil, err
 	}
+
 	if messageLength < 0 {
 		return nil, fmt.Errorf("Get model dictionary error: %s\n", ARTM_ERRORS[-messageLength])
 	}
+
 	buffer := make([]byte, messageLength)
-	errorID := C.ArtmCopyRequestedMessage(messageLength, C.CString(string(buffer)))
-	if len(errorStr) > 0 {
-		return nil, fmt.Errorf("Get model dictionary error: %s\n", errorStr)
+
+	errorID := C.ArtmCopyRequestedMessage(messageLength, (*C.char)(unsafe.Pointer(&buffer[0])))
+	err = ArtmGetLastErrorMessage()
+	if err != nil {
+		return nil, err
 	}
 	if errorID < 0 {
-		return nil, fmt.Errorf("Get dictionary data error: %s\n", ARTM_ERRORS[-errorID])
+		return nil, fmt.Errorf("Copy requested model dictionary: %s\n", ARTM_ERRORS[-errorID])
 	}
 
+	fmt.Printf("[%#v]", buffer[:10])
+
 	dictionaryData := &DictionaryData{}
+
 	err = proto.Unmarshal(buffer, dictionaryData)
 	if err != nil {
-		return nil, fmt.Errorf("protobuf DictionaryData unmarshaling error: ", err)
+		return nil, fmt.Errorf("Protobuf DictionaryData unmarshaling error: %s", err)
 	}
+
 	return dictionaryData, nil
+}
+
+func ArtmDisposeDictionary(masterModelID int, dictionaryName string) error {
+	bytesPointer := []byte(dictionaryName)
+	errorID := C.ArtmDisposeDictionary(C.int(masterModelID), (*C.char)(unsafe.Pointer(&bytesPointer[0])))
+	if errorID < 0 {
+		return fmt.Errorf("Dictionary dispose error: %s\n", ARTM_ERRORS[-errorID])
+	}
+	err := ArtmGetLastErrorMessage()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- artm_test.go
+++ artm_test.go
@@ -3,7 +3,7 @@ package goartm
 import "testing"
 
 func TestCreateMainComponent(t *testing.T) {
-	conf := &MasterModelConfig{}
+	conf := New(&MasterModelConfig{}).(*MasterModelConfig)
 	modelID, err := ArtmCreateMasterModel(conf)
 	if err != nil {
 		t.Fatal(err)
@@ -18,7 +18,8 @@ func TestCreateMainComponent(t *testing.T) {
 
 func TestArtmImportModel(t *testing.T) {
 	// initialize
-	conf := &MasterModelConfig{}
+	conf := New(&MasterModelConfig{}).(*MasterModelConfig)
+
 	modelID, err := ArtmCreateMasterModel(conf)
 	if err != nil {
 		t.Fatal(err)
@@ -26,7 +27,7 @@ func TestArtmImportModel(t *testing.T) {
 	t.Logf("Successfully created master model with id %d", modelID)
 	// load model
 	model := "model"
-	importModelConfig := &ImportModelArgs{}
+	importModelConfig := New(&ImportModelArgs{}).(*ImportModelArgs)
 	importModelConfig.FileName = &model
 	err = ArtmImportModel(modelID, importModelConfig)
 	if err != nil {
@@ -43,7 +44,7 @@ func TestArtmImportModel(t *testing.T) {
 
 func TestArtmRequestDictionary(t *testing.T) {
 	// initialize
-	conf := &MasterModelConfig{}
+	conf := New(&MasterModelConfig{}).(*MasterModelConfig)
 	modelID, err := ArtmCreateMasterModel(conf)
 	if err != nil {
 		t.Fatal(err)
@@ -51,23 +52,50 @@ func TestArtmRequestDictionary(t *testing.T) {
 	t.Logf("Successfully created master model with id %d", modelID)
 	// load model
 	model := "model"
-	importModelConfig := &ImportModelArgs{}
+	importModelConfig := New(&ImportModelArgs{}).(*ImportModelArgs)
 	importModelConfig.FileName = &model
 	err = ArtmImportModel(modelID, importModelConfig)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Logf("Successfully imported model into %d", modelID)
+	// load dictionary
+
+	importDictionaryArgsConfig := New(&ImportDictionaryArgs{}).(*ImportDictionaryArgs)
+	dicFile := "dictionary.dict"
+	dicName := "main_dictionary"
+	importDictionaryArgsConfig.FileName = &dicFile
+	importDictionaryArgsConfig.DictionaryName = &dicName
+	err = ArtmImportDictionary(modelID, importDictionaryArgsConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Successfully loaded dictionary into model %d", modelID)
+
 	// get dictionary
-	dictionaryConfig := &GetDictionaryArgs{}
+	dictionaryConfig := New(&GetDictionaryArgs{}).(*GetDictionaryArgs)
+	dictionaryConfig.DictionaryName = &dicName
 	dic, err := ArtmRequestDictionary(modelID, dictionaryConfig)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Logf("Successfully requested dictionary of model %d", modelID)
 
-	t.Logf("Dictionary name is [%s]", dic.Name)
+	t.Logf("Dictionary name is [%s]", *dic.Name)
 
+	t.Logf("Dictionary has %d words", len(dic.Token))
+	for i, token := range dic.Token {
+		t.Logf("Dictionary has %d = %s", i, token)
+		if i == 10 {
+			break
+		}
+	}
+	// dispose dictionary
+	err = ArtmDisposeDictionary(modelID, dicName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Successfully disposed model %d dictionary %s", modelID, dicName)
 	// dospose
 	err = ArtmDisposeMasterComponent(modelID)
 	if err != nil {
